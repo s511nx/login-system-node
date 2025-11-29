ea216e05c7a1ccd84fc6472d7d3aca31cdbb9e37
s511nx <sultan.otaibia@gmail.com>
2025-11-29 03:28:25 +0300

    Use dynamic PORT for Render

--- server.js
+++ server.js
@@ -121,6 +121,7 @@ app.get("/profile" , auth , (req , res) =>  {
 
     });
 });
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
     console.log("Server is running on port 3000");
 });
